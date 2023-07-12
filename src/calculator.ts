f46745eeaa7edfbc1a22da3c6344ce8d9f9ecd78
Zachattack221 <zsvd09@gmail.com>
2023-07-12 10:42:37 -0400

    created a new instance of the SimpleCalculator class and tested methods on calculator object

--- src/calculator.ts
+++ src/calculator.ts
@@ -37,3 +37,12 @@ divide() {
   }
   return this.num1 + this.num2;
 }
+
+// Creates a new instance of the SimpleCalculator class
+const calculator = new SimpleCalculator(6, 3);
+
+// Calls the methods on the calculator object and outputs a result
+console.log(calculator.add()); // Output: 9
+console.log(calculator.subtract()); // Output: 3
+console.log(calculator.multiply()); // Output: 18
+console.log(calculator.divide()); // Output: 2
