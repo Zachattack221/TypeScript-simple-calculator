d28377c92d1f2827d806de4a2a2d50a5599a2b56
Zachattack221 <zsvd09@gmail.com>
2023-07-12 10:00:16 -0400

    began to establish ts calculator interface and a class to implement iterations of said interface

---
+++ src/calculator.ts
@@ -0,0 +1,39 @@
+// Establishes an interface for calculator object
+interface Calculator {
+  num1: number;
+  num2: number;
+  add(): number;
+  subtract(): number;
+  multiply(): number;
+  divide(): number;
+}
+
+// Create a class to implement the Calculator interface
+class SimpleCalculator implements Calculator {
+  num1: number;
+  num2: number;
+}
+
+constructor(num1: number, num2: number) {
+  this.num1 = num1;
+  this.num2 = num2;
+}
+
+add() {
+  return this.num1 + this.num2;
+}
+
+subtract() {
+  return this.num1 - this.num2;
+}
+
+multiply() {
+  return this.num1 * this.num2;
+}
+
+divide() {
+  if (this.num2 === 0) {
+    throw new Error("Cannot divide by zero");
+  }
+  return this.num1 + this.num2;
+}
